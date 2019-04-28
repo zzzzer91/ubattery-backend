6c8e50a73679ea88084a90a6193911f04ce597e8
zzzzer <zzzzer91@gmail.com>
2019-04-28 12:16:19 +0800

    v0.05

--- ubattery/apis/v1/resources.py
+++ ubattery/apis/v1/resources.py
@@ -2,6 +2,7 @@ from flask import jsonify, request
 from flask.views import MethodView
 
 from ubattery.db import get_db
+from ubattery.common.mapping import LABEL_TO_NAME
 from ubattery.blueprints.auth import login_required
 
 
@@ -12,21 +13,25 @@ class AnalysisAPI(MethodView):
         args = request.args
 
         # TODO 后台参数合法性检查
-        start_date = args.get('startDate', '2015-1-1 1:1:1')
+        start_date = args.get('startDate', '2017-1-1 0:0:0')
         data_limit = int(args.get('dataLimit', 500))
+        need_params = args.get('needParams')
+
+        col_names = ['时间']
+
+        if need_params is None or not need_params.strip():
+            need_params = ''
+        else:
+            for k in need_params.split(','):
+                col_names.append(LABEL_TO_NAME[k])
+            need_params = ',' + need_params
 
         with get_db().cursor() as cursor:
             cursor.execute(
                 'SELECT '
-                'DATE_FORMAT(timestamp, \'%%Y-%%m-%%d %%H:%%i:%%s\'),'
-                'bty_t_vol,'
-                'bty_t_curr,'
-                'met_spd,'
-                's_b_max_t,'
-                's_b_min_t,'
-                's_b_max_v,'
-                's_b_min_v'
-                ' FROM vehicle1 '
+                'DATE_FORMAT(timestamp, \'%%Y-%%m-%%d %%H:%%i:%%s\')'
+                f'{need_params} '
+                'FROM vehicle1 '
                 'WHERE timestamp > %s '
                 'LIMIT %s',
                 (start_date, data_limit)
@@ -36,10 +41,7 @@ class AnalysisAPI(MethodView):
         return jsonify({
             'status': True,
             'data': {
-                'col_names': [
-                    '时间', '总电压', '总电流', '车速',
-                    '单体最高温度', '单体最低温度', '单体最高电压', '单体最低电压'
-                ],
+                'col_names': col_names,
                 'rows': rows
             }
         })
