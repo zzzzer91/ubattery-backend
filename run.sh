5f521929be1ad96eedf1680945f3d86634d82709
zzzzer <zzzzer91@gmail.com>
2019-07-11 19:30:37 +0800

    v0.98

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ docker-compose up -d
 
 # --log-file <file_name> 输出到指定文件
 # -D 后台运行
-gunicorn wsgi:app -w 3 -b unix:/tmp/ubattery.sock -k eventlet
+gunicorn wsgi:app -w 3 -b unix:/tmp/ubattery.sock -k eventlet -D
