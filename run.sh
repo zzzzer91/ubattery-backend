b728ab5680380f3af7ba6bf5f41dc129d5493b2c
zzzzer <zzzzer91@gmail.com>
2019-07-12 09:56:54 +0800

    v0.99

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 export FLASK_ENV=production
 
+# 启动数据库
 docker-compose up -d
 
 # 前台运行测试
 # gunicorn wsgi:app -w 1 -b 0:4000 -k eventlet
 
+# 使用本地套接字，需要 Nginx 作为服务器
 # --log-file <file_name> 输出到指定文件
 # -D 后台运行
 gunicorn wsgi:app -w 3 -b unix:/tmp/ubattery.sock -k eventlet -D
