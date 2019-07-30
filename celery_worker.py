fc1f21185756adefa0be57f567e2aef97e68d586
zzzzer <zzzzer91@gmail.com>
2019-07-30 15:50:25 +0800

    v1.1

--- celery_worker.py
+++ celery_worker.py
@@ -1,6 +1,7 @@
 """用于运行 celery worker，单独创建一个文件是为了提供上下文环境"""
 
 from ubattery import create_app
+# 导出给 celery 使用
 from ubattery.extensions import celery
 
 app = create_app()
--- run-celery-worker.sh
+++ run-celery-worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A celery_worker.celery worker --loglevel=INFO --concurrency=4
+celery -A celery_worker.celery worker --loglevel=INFO --concurrency=2
