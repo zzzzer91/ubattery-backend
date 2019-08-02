c41386dd8c6053e8453bd176134af9665c99bf6c
zzzzer <zzzzer91@gmail.com>
2019-08-02 16:16:59 +0800

    v1.1 fix ip bug

--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@ export FLASK_ENV=production
 docker-compose up -d
 
 # 前台运行测试
-# gunicorn wsgi:app -w 1 -b 172.18.0.1:5000 -k eventlet
+# gunicorn wsgi:app -w 1 -b localhost:5000 -k eventlet
 
 # 使用本地套接字，需要 Nginx 作为服务器
 # --log-file <file_name> 输出到指定文件
 # -D 后台运行
-gunicorn wsgi:app -w 3 -b 172.18.0.1:5000 -k eventlet -D
+gunicorn wsgi:app -w 3 -b localhost:5000 -k eventlet -D
 
 # 创建后台任务执行者
 # -A 指定 celery 实例位置
